005b39b300adb4579068d52f15f8f686d609ccf0
sankur-codes <ankursingh1805@gmail.com>
2021-06-23 11:04:43 +0530

    Adding typing animation

--- js/index.js
+++ js/index.js
@@ -11,3 +11,11 @@ navLinks.forEach(link => {
   })
   
 });
+
+// typing animation 
+var typed = new Typed(".section__subtitle--about--Profiles", {
+  strings: ["Developer", "Learner", "DevOps Engineer", "Researcher", "Hiker"],
+  typeSpeed: 100,
+  backSpeed: 60,
+  loop: true
+})
