6db1ee2e6f65c389439e493215657918a34cebc1
sankur-codes <ankursingh1805@gmail.com>
2021-06-27 20:32:04 +0530

    Styling and TopBtn

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelectorAll('.nav__link')
+var mybutton = document.getElementById("ToTop");
 
 navToggle.addEventListener('click', () => {
   document.body.classList.toggle('nav-open');
@@ -19,3 +20,18 @@ var typed = new Typed(".section__subtitle--about--Profiles", {
   backSpeed: 60,
   loop: true
 })
+
+// slide up script 
+
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.opacity = 1;
+  } else {
+    mybutton.style.opacity = 0;
+  }
+}
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
